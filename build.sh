d01829ff8bd13d61249a45d6f3c50ed8067ed37f
swissChili <swisschili@fastmail.com>
2020-04-18 12:28:46 -0700

    Fix build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ sudo make aab -j9
 # move the apk to here
 mv android-build/build/outputs/apk/debug/android-build-debug.apk qtodo.apk
 
+echo Releasing under name `git tag | tail -n 1`
+
 # release the APK on github
 # (github key in .env)
-releaseme/releaseme.sh -n `git tag | head -n 1` -f qtodo.apk -m application/zip -r swissChili/qtodo
+releaseme/releaseme.sh -n `git tag | tail -n 1` -f qtodo.apk -m application/zip -r swissChili/qtodo
